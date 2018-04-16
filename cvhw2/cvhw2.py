19fbf75f1982eaf8a6a13a85f0a0e5a0f638b975
Ebubekir Çağrı ŞEN <cagrisen_snt@hotmail.com>
2018-04-16 21:33:19 +0300

    okey test

--- cvhw2/cvhw2.py
+++ cvhw2/cvhw2.py
@@ -7,11 +7,13 @@ def onCannyParamChange(x):
     pass
 
 def main():
-    img = cv2.imread("twoimage.jpg")
+    img = cv2.imread("istaka.jpg")
     img_o = img.copy()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ret,thresh = cv2.threshold(gray, 127,255,0)
     img = cv2.resize(img, None, None, 1, 1, cv2.INTER_CUBIC)
     img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    img = cv2.equalizeHist(img)
+    #img = cv2.equalizeHist(img)
 
     clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
     #img = clahe.apply(img)
@@ -42,6 +44,9 @@ def main():
     local_bin_img = cv2.medianBlur(local_bin_img, 11)
     local_bin_img = cv2.medianBlur(local_bin_img, 3)
 
+    cv2.imshow("tetest", global_bin_img)
+    cv2.imshow("tetest2", gray)
+
     #cv2.imshow("Original Image", img_o)
     #cv2.imshow("Canny Edges", cannyEdges)
     #cv2.imshow("Gray image", img)
@@ -57,13 +62,17 @@ def main():
         cannyMaxVal = cv2.getTrackbarPos("cannyMax\n1", "Canny Edges")
         sizefactor = cv2.getTrackbarPos("sizefactor\n1", "Canny Edges") / 100
 
-        img = cv2.resize(local_bin_img, None, None, sizefactor,sizefactor, cv2.INTER_CUBIC)
+        img = cv2.resize(global_bin_img, None, None, sizefactor,sizefactor, cv2.INTER_CUBIC)
 
-        cannyEdges = cv2.Canny(img,cannyMinVal,cannyMaxVal)
-        sobel = cv2.Sobel(local_bin_img, cv2.CV_64F,1,1,ksize=5)
-
-        cv2.imshow("sobel", sobel)
-        cv2.imshow("Canny Edges", cannyEdges)
+        #cannyEdges = cv2.Canny(gray,cannyMinVal,cannyMaxVal)
+        
+        kontur, contours, hierarchy = cv2.findContours(img,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+        cv2.drawContours(kontur, contours, -1, (0,255,0), 3)
+        x,y,w,h = cv2.boundingRect(contours[0])
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+        cv2.imshow("kontur", kontur)
+        cv2.imshow("img", img)
+        #cv2.imshow("Canny Edges", cannyEdges)
         k = cv2.waitKey(1) & 0xFF
         if k == 27:
             break
